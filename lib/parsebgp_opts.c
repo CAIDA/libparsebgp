cb832b83f4aba7ae5a20ebab467a0356d4ba2cab
Alistair King <alistair@caida.org>
2017-09-20 15:08:43 -0700

    Fix small unitialized bug in options

--- lib/parsebgp_opts.c
+++ lib/parsebgp_opts.c
@@ -1,9 +1,10 @@
 #include "parsebgp_opts.h"
+#include <string.h>
 
 void parsebgp_opts_init(parsebgp_opts_t *opts)
 {
-  // TODO: allow the default for this to be configured at compile time
-  opts->ignore_not_implemented = 0;
+  // TODO: allow the default for some of these to be configured at compile time
+  memset(opts, 0, sizeof(*opts));
 
   parsebgp_bgp_opts_init(&opts->bgp);
 }
