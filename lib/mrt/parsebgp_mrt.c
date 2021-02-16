e518af2402253904132b6a13eab63ccd006d22d0
dteach <dteach@uoreogn.edu>
2021-02-15 23:02:19 -0800

    Clean up fallthrough switch case.

--- lib/mrt/parsebgp_mrt.c
+++ lib/mrt/parsebgp_mrt.c
@@ -774,9 +774,6 @@ static parsebgp_error_t parse_bgp4mp(parsebgp_opts_t *opts,
     DESERIALIZE_IP(msg->afi, buf, len, nread, msg->local_ip);
   }
 
-  // Reset add_path for each message
-  opts->bgp.add_path = 0;
-
   // And then the actual data, based on the subtype
   // the _AS4 subtypes actually only change the common part of the message, so
   // we can treat them the same as their non-AS4 subtype at this point.
@@ -791,34 +788,36 @@ static parsebgp_error_t parse_bgp4mp(parsebgp_opts_t *opts,
     break;
 
   case PARSEBGP_MRT_BGP4MP_MESSAGE_AS4:
-  case PARSEBGP_MRT_BGP4MP_MESSAGE_AS4_ADDPATH:
   case PARSEBGP_MRT_BGP4MP_MESSAGE_AS4_LOCAL:
+    opts->bgp.asn_4_byte = 1;
+    opts->bgp.add_path = 0;
+    break;
+  case PARSEBGP_MRT_BGP4MP_MESSAGE_AS4_ADDPATH:
   case PARSEBGP_MRT_BGP4MP_MESSAGE_AS4_LOCAL_ADDPATH:
     opts->bgp.asn_4_byte = 1;
     opts->bgp.add_path = 1;
-  // FALL THROUGH
-
+    break;
   case PARSEBGP_MRT_BGP4MP_MESSAGE_ADDPATH:
   case PARSEBGP_MRT_BGP4MP_MESSAGE_LOCAL_ADDPATH:
-  // FALL THROUGH
+    opts->bgp.asn_4_byte = 0;
     opts->bgp.add_path = 1;
-
+    break;
   case PARSEBGP_MRT_BGP4MP_MESSAGE_LOCAL:
   case PARSEBGP_MRT_BGP4MP_MESSAGE:
-    PARSEBGP_MAYBE_MALLOC_ZERO(msg->data.bgp_msg);
-    slen = len - nread;
-    err = parsebgp_bgp_decode_ext(opts, msg->data.bgp_msg, buf, &slen, 1);
-    if (err != PARSEBGP_OK && err != PARSEBGP_TRUNCATED_MSG) {
-      return err;
-    }
-    nread += slen;
-    buf += slen;
     break;
 
   default:
     PARSEBGP_RETURN_INVALID_MSG_ERR;
     break;
   }
+  PARSEBGP_MAYBE_MALLOC_ZERO(msg->data.bgp_msg);
+  slen = len - nread;
+  err = parsebgp_bgp_decode_ext(opts, msg->data.bgp_msg, buf, &slen, 1);
+  if (err != PARSEBGP_OK && err != PARSEBGP_TRUNCATED_MSG) {
+    return err;
+  }
+  nread += slen;
+  buf += slen;
 
   *lenp = nread;
   return err;
